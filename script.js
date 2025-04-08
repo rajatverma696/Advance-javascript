8aa486eb630c1b1e0845f015d7191ab669dcef96
rajatverma696 <rajat696verma@gmail.com>
2025-04-08 13:06:22 +0530

    third day

--- script.js
+++ script.js
@@ -35,6 +35,7 @@
 // addTwoNumbers();
 
 //---------Arrow functions are great for sending functions around as a parameters and used as shortcut notation
+//FORMAT = (parameter1, parameter2) => body of the function;
 
 // (p1,p2) => console.log(p1+p2);
 
@@ -53,6 +54,61 @@
 // const arr = ["squirrel", "alpaca", "buddy"];
 // arr.forEach(e => console.log(e));
 
+//The spread operator ... in JavaScript expands an iterable (like an array, string, or object) into individual elements
+
 // let spread = ["so","much","fun"];
 // let message = ["JS", "is", ...spread, "and", "very", "powerful"];
+// let message1 = ["Science", "is", ...spread, "and", "very", "important", "Subject"];
 // console.log(message);
+// console.log(...message);
+// console.log(message1);
+
+// let greet = ["hello", "baby,", "playing", "with", "you", "is ",...spread];
+// console.log(...greet);
+
+// function addTwoNumbers(x,y){
+//     console.log(x+y);
+// }
+// let arr = [5,8];
+// addTwoNumbers(...arr);
+
+// function addFourNumbers(x,y,z,a){
+//     console.log(x+y+z+a);
+// }
+// let arr = [5,9];
+// let arr2 = [6,7];
+
+// addFourNumbers(...arr, ...arr2);
+
+// REST PARAMETER INN SPREAD FUNCTION: 
+
+// function someFunction(para1,...para2){
+//     console.log(para1,para2);
+// }
+// someFunction("hi", "there", "How are you");
+
+// function addTwoNumbers(x,y){
+//        console.log(x+y);
+//      }
+    //  let result = addTwoNumbers(4,5);
+    //  console.log(result);
+
+    //  let addTwoNumbers = (x,y) => console.log(x+y);
+
+    //  let resultArr = [];
+    //  for(let i =0; i<10; i++){
+    //     let result = addTwoNumbers(i, i*2);
+    //     resultArr.push(result);
+    //  }
+    //  console.log(resultArr);
+
+    //RETURNING WITH ARROW FUNCTIONS:
+
+    let addTwoNumbers = (x,y) => x+y;
+    let resultArr = [];
+    for(let i =0; i<10; i++){
+       let result = addTwoNumbers(i, i*2);
+       resultArr.push(result);
+    }
+    console.log(resultArr);
+
